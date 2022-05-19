cd3c43e3b1fd5de8c284cc5493cfcadd67461a7c
BryonVerdone <bryonverdone@gmail.com>
2022-05-18 20:18:15 -0400

    switch product images with btns

--- src/app/pages/products/product.jsx
+++ src/app/pages/products/product.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-
-export default function Product({ name, image, size, styles, colors }) {
+import React, { useState } from 'react';
+import './styles.css';
+export default function Product({ name, size, styles, colors }) {
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
   return (
     <div className='product'>
       <div className='name'>{name}</div>
-      {colors.map((color) => (
-        <div className='color'>
-          <img width={100} src={color.image} alt={`image for ${name}`} />
-        </div>
-      ))}
+      <div className='colors'>
+        Available in:
+        {colors.map((color) => (
+          <span
+            className={`color ${selectedColor === color ? 'selected' : null}`}
+            onClick={() => setSelectedColor(color)}
+          >
+            {color.name}
+          </span>
+        ))}
+      </div>
+      <img
+        width={400}
+        height={400}
+        src={selectedColor.image}
+        alt={`image for ${name}`}
+      />
     </div>
   );
 }
